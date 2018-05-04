c3aad8e6382279fd5ceaad8e6da24ff3e33d774e
Marwa Abdulhai <abdulhai@mit.edu>
2018-05-04 11:26:29 -0400

    changed piece playing tests

--- test/karaoke/sound/SequencePlayerTest.java
+++ test/karaoke/sound/SequencePlayerTest.java
@@ -40,13 +40,13 @@ public class SequencePlayerTest {
 
         // create a new player
         final int beatsPerMinute = 140; // a beat is a quarter note, so this is 120 quarter notes per minute
-        final int ticksPerBeat = 64; // allows up to 1/64-beat notes to be played with fidelity
+        final int ticksPerBeat = 12; // allows up to 1/64-beat notes to be played with fidelity
         SequencePlayer player = new MidiSequencePlayer(beatsPerMinute, ticksPerBeat);
         
         // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
         // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
         
-        int startBeat = 0;
+        double startBeat = 0;
         player.addNote(piano, new Pitch('C'), startBeat, 1.0);
         player.addNote(piano, new Pitch('C'), startBeat +=1.0, 1.0);
         player.addNote(piano, new Pitch('C'), startBeat +=1.0, 3.0/4);
@@ -80,7 +80,7 @@ public class SequencePlayerTest {
         player.addNote(piano, new Pitch('D'), startBeat +=3.0/4, 1.0/4);
         player.addNote(piano, new Pitch('C'), startBeat +=1.0/4, 2.0);
 
-        
+
         
         // add a listener at the end of the piece to tell main thread when it's done
         Object lock = new Object();
@@ -96,7 +96,6 @@ public class SequencePlayerTest {
         // play!
         player.play();
         
-
         // wait until player is done
         // (not strictly needed here, but useful for JUnit tests)
         synchronized (lock) {
@@ -107,6 +106,7 @@ public class SequencePlayerTest {
             }
         }
         System.out.println("done playing");
+
     }
     
     /**
@@ -127,7 +127,7 @@ public class SequencePlayerTest {
         // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
         // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
         
-        int startBeat = 0;
+        double startBeat = 0;
         //[e/2 ^F/2] [e/2 F/2] z/2 [e/2 ^F/2] z/2 [c/2 F/2] [e F] |[g B G] z G z | c3/2 G/2 z E | E/2 A B  _B/2 A | (3GeG a f/2 g/2 |z/2 e c/2 d/2 B3/4        
 
         player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, 1.0/2);
@@ -142,14 +142,14 @@ public class SequencePlayerTest {
         player.addNote(piano, new Pitch('C').transpose(Pitch.OCTAVE), startBeat+=(1.0/2 + 1.0/2), 1.0/2); //added rest
         player.addNote(piano, new Pitch('F'), startBeat, 1.0/2);
         
-        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat+=(1.0/2), 1);
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat+=(1.0/2), 1.0);
         player.addNote(piano, new Pitch('F'), startBeat, 1.0);
         
-        player.addNote(piano, new Pitch('G').transpose(Pitch.OCTAVE), startBeat+=1.0, 1);
-        player.addNote(piano, new Pitch('B'), startBeat, 1);
-        player.addNote(piano, new Pitch('G'), startBeat, 1);
+        player.addNote(piano, new Pitch('G').transpose(Pitch.OCTAVE), startBeat+=1.0, 1.0);
+        player.addNote(piano, new Pitch('B'), startBeat, 1.0);
+        player.addNote(piano, new Pitch('G'), startBeat, 1.0);
         
-        player.addNote(piano, new Pitch('G'), startBeat+=(1.0 + 1.0), 1); //added rest
+        player.addNote(piano, new Pitch('G'), startBeat+=(1.0 + 1.0), 1.0); //added rest
         
         player.addNote(piano, new Pitch('C'), startBeat+=(1.0 + 1.0), 3.0/2); //added rest
         player.addNote(piano, new Pitch('G'), startBeat+=3.0/2, 1.0/2); 
@@ -213,47 +213,47 @@ public class SequencePlayerTest {
         List<String> words = Arrays.asList("A -", " ma - zing_ "," grace! How "," sweet the "," sound That" ," saved a "," wretch like ","me.");
         // create a new player
         final int beatsPerMinute = 100; // a beat is a quarter note, so this is 120 quarter notes per minute
-        final int ticksPerBeat = 512; // allows up to 1/64-beat notes to be played with fidelity
+        final int ticksPerBeat = 100; // allows up to 1/64-beat notes to be played with fidelity
         SequencePlayer player = new MidiSequencePlayer(beatsPerMinute, ticksPerBeat);
         
         // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
         // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
         //z4 D2 | G4 B G | B4 A2 | G4 E2 | D4 D2 | G4 B G | B4 A2 | d6
         //w: A - | ma - zing_ | grace! How | sweet the | sound ThSat | saved a_ | wretch like | me.
-        int startBeat = 0;
+        double startBeat = 0;
         
         player.addEvent(startBeat, (Double beat) -> { System.out.print("A - "); });
-        player.addNote(piano, new Pitch('D'), startBeat+=4, 2);
+        player.addNote(piano, new Pitch('D'), startBeat+=4.0, 2.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("ma - zing "); });
-        player.addNote(piano, new Pitch('G'), startBeat +=2, 4);
-        player.addNote(piano, new Pitch('B'), startBeat +=4, 1);
-        player.addNote(piano, new Pitch('G'), startBeat +=1, 1);
+        player.addNote(piano, new Pitch('G'), startBeat +=2.0, 4.0);
+        player.addNote(piano, new Pitch('B'), startBeat +=4.0, 1.0);
+        player.addNote(piano, new Pitch('G'), startBeat +=1.0, 1.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("grace! How "); });
-        player.addNote(piano, new Pitch('B'), startBeat +=1, 4);
-        player.addNote(piano, new Pitch('A'), startBeat+=4, 2);
+        player.addNote(piano, new Pitch('B'), startBeat +=1.0, 4.0);
+        player.addNote(piano, new Pitch('A'), startBeat+=4.0, 2.0);
 
         
         player.addEvent(startBeat, (Double beat) -> { System.out.print("sweet the "); });
-        player.addNote(piano, new Pitch('G'), startBeat +=2, 4);
-        player.addNote(piano, new Pitch('E'), startBeat +=4, 2);
+        player.addNote(piano, new Pitch('G'), startBeat +=2.0, 4.0);
+        player.addNote(piano, new Pitch('E'), startBeat +=4.0, 2.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("sound That "); });
-        player.addNote(piano, new Pitch('D'), startBeat +=2, 4);
-        player.addNote(piano, new Pitch('D'), startBeat +=4, 2);
+        player.addNote(piano, new Pitch('D'), startBeat +=2.0, 4.0);
+        player.addNote(piano, new Pitch('D'), startBeat +=4.0, 2.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("saved a "); });
-        player.addNote(piano, new Pitch('G'), startBeat +=2, 4);
-        player.addNote(piano, new Pitch('B'), startBeat +=4, 1);
-        player.addNote(piano, new Pitch('G'), startBeat +=1, 1);
+        player.addNote(piano, new Pitch('G'), startBeat +=2.0, 4.0);
+        player.addNote(piano, new Pitch('B'), startBeat +=4.0, 1.0);
+        player.addNote(piano, new Pitch('G'), startBeat +=1.0, 1.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("wretch like "); });
-        player.addNote(piano, new Pitch('B'), startBeat +=1, 4);
-        player.addNote(piano, new Pitch('A'), startBeat +=4, 2);
+        player.addNote(piano, new Pitch('B'), startBeat +=1.0, 4.0);
+        player.addNote(piano, new Pitch('A'), startBeat +=4.0, 2.0);
 
         player.addEvent(startBeat, (Double beat) -> { System.out.print("me."); });
-        player.addNote(piano, new Pitch('D').transpose(Pitch.OCTAVE), startBeat+=2, 6);
+        player.addNote(piano, new Pitch('D').transpose(Pitch.OCTAVE), startBeat+=2.0, 6.0);
 
     
         // add a listener at the end of the piece to tell main thread when it's done
